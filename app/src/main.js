a2b388870565baee1662d735083047bf903564a9
jaimersoncorreia <jaimerson_correia@hotmail.com>
2019-11-23 16:52:42 -0400

    Subindo a aula completa de Aplicação com ES6+

--- app/src/main.js
+++ app/src/main.js
@@ -1,24 +1,67 @@
+import api from "./api";
+
 class App {
     constructor() {
         this.repositories = [];
         this.formEl = document.getElementById("repo-form");
+        this.inputEl = document.querySelector("input[name=repository]");
         this.listEl = document.getElementById("repo-list");
 
         this.registerHandlers();
     }
+
     registerHandlers() {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
-    addRepository(event) {
+
+    setLoading(loading = true) {
+        if (loading === true) {
+            let loadingEl = document.createElement("span");
+            loadingEl.appendChild(document.createTextNode(" Carregando..."));
+            loadingEl.setAttribute("id", "loading");
+
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById("loading").remove();
+        }
+    }
+
+    async addRepository(event) {
         event.preventDefault();
 
-        this.repositories.push({
-            name: "Jaimerson Correia",
-            description: "Apenas um hobby",
-            avatar_url: "https://avatars1.githubusercontent.com/u/17113568?v=4",
-            html_url: "https://github.com/jaimersoncorreia"
-        });
-        this.render();
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0) {
+            return;
+        }
+
+        this.setLoading();
+
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
+            //console.log(response);
+            const {
+                name,
+                description,
+                html_url,
+                owner: { avatar_url }
+            } = response.data;
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url
+            });
+
+            this.render();
+        } catch (error) {
+            alert("O repositório não existe");
+            console.log(error);
+        } finally {
+            this.inputEl.value = "";
+        }
+        this.setLoading(false);
     }
     render() {
         this.listEl.innerHTML = "";
@@ -37,6 +80,7 @@ class App {
 
             let linkEl = document.createElement("a");
             linkEl.setAttribute("target", "_blank");
+            linkEl.setAttribute("href", repo.html_url);
             linkEl.appendChild(document.createTextNode("Acessar"));
 
             let listItemEl = document.createElement("li");
