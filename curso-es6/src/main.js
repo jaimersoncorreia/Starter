7479b515d27fa006017223d48b0fac03059effc4
jaimersoncorreia <jaimerson_correia@hotmail.com>
2019-11-22 21:22:07 -0400

    Subindo aula 1 do Async/Await

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,1 +1,19 @@
-alert("Testando");
+const minhaPromise = () =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("ok");
+        }, 2000);
+    });
+
+/* async function executaPromise() {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+} */
+const executaPromise = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+};
+
+executaPromise();
